da427e070940d0a4a1986b6025b1988e0e91665d
Alikhan Beisenbayev <rkoenigseggagera@gmail.com>
2025-03-01 13:36:54 +0100

    fixed app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import "./styles/main.scss";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import StoryPage from "./pages/StoryPage/StoryPage";
-import AboutPage from "./pages/AboutPage/AboutPage";
 import SEO from './components/SEO';
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <SEO />
-      <Routes>
-        <Route path="/" element={<StoryPage />} />
-        <Route path="/about" element={<AboutPage />} />
-      </Routes>
-    </BrowserRouter>
+      <StoryPage />
+    </>
   );
 }
 
